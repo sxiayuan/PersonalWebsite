edce494fc4dd666a78fe747990bc0e9135864452
sxiayuan <xiasyl88@gmail.com>
2026-05-21 17:54:35 -0400

    general changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import path from 'node:path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/PersonalWebsite',
+  assetPrefix: '/PersonalWebsite/',
   turbopack: {
     root: path.resolve('.'),
   },
